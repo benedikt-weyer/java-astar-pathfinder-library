071feb9d76d55c0811194de38669b836349918d3
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2024-12-11 19:04:22 +0100

    removed unused imports

--- src/main/java/io/github/benediktweyer/astarpathfinder/AStarPathfinder.java
+++ src/main/java/io/github/benediktweyer/astarpathfinder/AStarPathfinder.java
@@ -1,9 +1,7 @@
 package io.github.benediktweyer.astarpathfinder;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
